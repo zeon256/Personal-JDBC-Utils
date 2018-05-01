59bf543a6866fa2cccc81e4baa49bc88178e2bb7
budisyahiddin <budisyahiddin@protonmail.ch>
2018-05-01 22:58:59 +0800

    Added docs for setAllValues

--- src/main/kotlin/com/budinverse/utils/experimental/Experimental.kt
+++ src/main/kotlin/com/budinverse/utils/experimental/Experimental.kt
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement
  * Performance issues with varargs at least according to
  * https://sites.google.com/a/athaydes.com/renato-athaydes/posts/kotlinshiddencosts-benchmarks
  *
+ * @param data
  */
 fun PreparedStatement.setAllValues(vararg data: Any) {
     for(i in 0 until data.size){ this[i+1] = data[i] }
